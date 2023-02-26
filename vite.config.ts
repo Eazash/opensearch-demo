e2d6af8cc74c0902c6d957e303047bbd3a566349
Ezira Ashenafi <eazash22@gmail.com>
2023-02-26 15:47:10 +0300

    chore: log env

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig, loadEnv } from 'vite';
 import { genOpensearchDescription } from './src/plugins/genOpensearch';
 export default defineConfig(({ mode }) => {
 	const env = loadEnv(mode, process.cwd(), '');
-	console.log(env.VITE_VERCEL_URL);
+	console.log(env);
 	return {
 		plugins: [
 			sveltekit(),
