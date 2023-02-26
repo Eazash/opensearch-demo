93da86c956b2bb20f9d8db0c13c5d9abf6fea933
Ezira Ashenafi <eazash22@gmail.com>
2023-02-26 15:42:54 +0300

    hotfix: attempt env prefix override

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig, loadEnv } from 'vite';
 import { genOpensearchDescription } from './src/plugins/genOpensearch';
 export default defineConfig(({ mode }) => {
-	const env = loadEnv(mode, process.cwd());
-
+	const env = loadEnv(mode, process.cwd(), '');
+	console.log(env.VITE_VERCEL_URL);
 	return {
 		plugins: [
 			sveltekit(),
 			genOpensearchDescription({
-				baseUrl: `https://${env.VERCEL_URL}`,
+				baseUrl: `https://${env.VITE_VERCEL_URL}`,
 				fileName: 'opensearch.xml',
 				shortName: 'Product Catalog',
 				longName: 'Demo Product Catalog with Opensearch spec',
